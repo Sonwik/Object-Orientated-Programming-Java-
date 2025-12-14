ba06a7741ceff64e81a1ca42c445c4face050e94
Zharkyn <zharkynabayev@MacBook-Air-Zharkyn.local>
2025-12-14 19:12:32 +0500

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,11 @@
+public class Main {
+    static void main() {
+        System.out.println("Hello World");
+        Rectangle a = new Rectangle();
+        a.setHeight(100);
+        a.width = 10000;
+        System.out.println(a.area());
 
-void main() {
-    System.out.println("Hello World");
+    }
+
 }
---
+++ src/Rectangle.java
@@ -0,0 +1,66 @@
+public class Rectangle{
+
+    public double width;
+    private double height;
+    private int id;
+    private static int idGen = 1;
+
+    // Default constructor
+    public Rectangle() {
+        this.width = 1.0;
+        this.height = 1.0;
+        this.id = idGen++;
+    }
+
+    // Constructor with parameters
+    public Rectangle(double width, double height) {
+        this();
+        setWidth(width);
+        setHeight(height);
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        if (width <= 0) {
+            throw new IllegalArgumentException("Width must be greater than 0");
+        }
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        if (height <= 0) {
+            throw new IllegalArgumentException("Height must be greater than 0");
+        }
+        this.height = height;
+    }
+
+    public double area() {
+        return width * height;
+    }
+
+    public double perimeter() {
+        return 2 * (width + height);
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "id=" + id +
+                ", width=" + width +
+                ", height=" + height +
+                '}';
+    }
+
+    static void main() {
+        Rectangle a = new Rectangle();
+        a.setHeight(-5);
+        System.out.println(a.height);
+    }
+}
